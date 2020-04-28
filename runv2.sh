a282f60995e241980327279633edaff12b2f6ecc
anserme <lczr1026673211@gmail.com>
2020-04-28 05:23:54 -0500

    Update runv2.sh

--- runv2.sh
+++ runv2.sh
@@ -1,26 +1,34 @@
 #!/usr/bin/env bash
 if [ ! -z "${bayeport}" ]
     then
-          sed -i "s/mtport/${port}/g"  "/home/tgproxy/config.py"
+          sed -i "s/bayeport/${port}/g"  "/etc/v2ray/config.json"
 fi
 if [ ! -z "${bayelisten}" ]
     then
-         sed -i "s/\"mtuser\"/\"${user}\"/g" "/home/tgproxy/config.py"
+         sed -i "s/\"bayelisten\"/\"${user}\"/g" "/etc/v2ray/config.json"
 fi
 if [ ! -z "${bayeid}" ]
     then
-         sed -i "s/\"00000000000000000000000000000001\"/\"${key}\"/g" "/home/tgproxy/config.py"
+         sed -i "s/\"bayeid\"/\"${key}\"/g" "/etc/v2ray/config.json"
 fi
 if [ ! -z "${bayealter}" ]
     then
-         sed -i "s/3c09c680b76ee91a4c25ad51f742267d/${tag}/g" "/home/tgproxy/config.py"
+         sed -i "s/bayealter/${alter}/g" "/etc/v2ray/config.json"
 fi
 if [ ! -z "${bayenet}" ]
     then
-         sed -i "s|\"www.google.com\"|\"${domain}\"|g" "/home/tgproxy/config.py"
+         sed -i "s/\"bayenet\"/\"${net}\"|g" "/etc/v2ray/config.json"
 fi
 if [ ! -z "${bayepath}" ]
     then
-         sed -i "s|\"www.google.com\"|\"${domain}\"|g" "/home/tgproxy/config.py"
+         sed -i "s/\"bayepath\"/\"${path}\"|g" "/etc/v2ray/config.json"
 fi
+
+acme.sh --install-cert -d ${domain} --ecc \
+        --key-file       /etc/v2ray/v2ray.key \
+        --fullchain-file /etc/v2ray/v2ray.crt \
+        --reloadcmd     "service nginx force-reload"
+
+
+
 v2ray -config=/etc/v2ray/config.json
